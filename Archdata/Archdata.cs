94a286cf14d0268152f839643be966ad3b8671dd
RiotMesh <riotmesh@mail.ru>
2024-05-23 15:57:32 +0500

    initial, make lib skeleton

---
+++ Archdata/Archdata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiotMesh.Archdata
+{
+    public class Archdata
+    {
+        public Archdata() { }
+
+        public string Get() { return String.Empty; }
+
+        public void Set() { }
+
+        public void Add() { }
+    }
+}
--- Archdata/Modules/ADCryptor.cs
+++ Archdata/Modules/ADCryptor.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RiotMesh.Archdata
+namespace Archdata.Modules
 {
-    public class Class1
+    internal class ADCryptor
     {
-
+        // TODO
     }
 }
