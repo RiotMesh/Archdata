65610f06522a54bb93ded83278fa6f10b21ed265
RiotMesh <riotmesh@mail.ru>
2024-05-30 15:34:05 +0500

    add static skeleton

--- Archdata/Archdata.cs
+++ Archdata/Archdata.cs
@@ -12,8 +12,21 @@ namespace RiotMesh.Archdata
 
         public string Get() { return String.Empty; }
 
-        public void Set() { }
+        public int GetInt() { return 0; }
 
-        public void Add() { }
+        public bool GetBool() { return false; }
+
+        public float GetFloat() { return 0f; }
+    }
+
+    public class ArchdataFile
+    {
+        public static string Get() { return String.Empty; }
+
+        public static int GetInt() { return 0; }
+
+        public static bool GetBool() { return false; }
+
+        public static float GetFloat() { return 0f; }
     }
 }
